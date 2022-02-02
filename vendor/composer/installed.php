717503a352c61f773215a6c34c54c9ac13a540cd
Ross Alexander <rossaalexander@gmail.com>
2022-02-02 14:24:39 +0100

    Update sprout

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '77c8ec33101e2ebb8a6c697eb706c6ee220eef9b',
+        'name' => 'sprout/sprout',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sprout/sprout' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '77c8ec33101e2ebb8a6c697eb706c6ee220eef9b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
